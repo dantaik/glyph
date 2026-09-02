fefe78ed5d4dfc17c430b2f0a376aa06eb36e27f
Daniel Wang <dan@taiko.xyz>
2026-09-02 16:53:37 +0800

    ui: 刻第一篇 -> 写第一篇

--- webapp/src/components/HomeFeed.jsx
+++ webapp/src/components/HomeFeed.jsx
@@ -102,7 +102,7 @@ export default function HomeFeed({ navigate, onStartWriting }) {
         <EmptyState
           title="此刻还没有文章"
           body="最近的区块里没有发现新的文章。连接钱包，刻下第一篇。"
-          actionLabel="刻第一篇"
+          actionLabel="写第一篇"
           onAction={onStartWriting}
         />
       ) : (
